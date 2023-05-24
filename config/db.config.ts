64fbbc629f8a718807298583d90561c2d2ad9ae3
rolandm99 <manfulmweze99@gmail.com>
2023-05-24 11:26:39 +0200

    Create the configuration folders

---
+++ config/db.config.ts
@@ -0,0 +1,15 @@
+import { connect } from "mongoose";
+import { config } from "./config";
+
+export const connectDB = async () => {
+    try {
+        await connect(config.MONGO_DB_URL, {
+            keepAlive: true,
+            connectTimeoutMS: 80000,
+            socketTimeoutMS: 80000,
+        });
+        console.log(" Connection to the database established successfully");
+    } catch (error) {
+        console.log("Could not connect to the database  : ", error);
+    }
+};
