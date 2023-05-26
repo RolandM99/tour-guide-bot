99eb87653d027d04560ca7e0444bcc8918525dd9
rolandm99 <manfulmweze99@gmail.com>
2023-05-26 18:22:45 +0200

    Create the menu for the place categories

--- telegram/tg_bot.ts
+++ telegram/tg_bot.ts
@@ -1,14 +1,52 @@
-import { Telegraf } from 'telegraf';
-// import { config } from '../config/config';
-// import { connectDB } from '../config/db.config';
+import { connectDB } from '../app/config/db.config';
+const Telegraf = require("telegraf");
 
 /*
-@@Author: Roland Mweze
-TODO:
-1. Create a bot using the BotFather
-2. Get the bot token and add it to the .env file
-3. Implement the bot logic with a friendly interface using the Telegraf library
+@@ Author: Roland Mweze
 */
+
+connectDB();
+require("dotenv").config();
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
+const state: Record<string, any> = {};
+
+const placeCategory = ['Hotel 🏨', 'Restaurant/Bar 🍲', 'Park 🏞️', 'Church ⛪', 'Mosque 🕌', 'Cinema 🎥', 'Theater 🎭', 'Stadium 🏟️', 'Amusement Park 🎢', 'Library 📚', 'Night Club 🎆', 'Coffee Shop ☕', 'Supermarket 🏪', 'Hospital 🏥', 'Bank 🏦', 'University 🎓', 'Florist 🌹']
+
+const placeCategoryMenu = Telegraf.Extra.markdown().markup((m: any) => {
+    const keyboard = [];
+  
+    for (let i = 0; i < placeCategory.length; i += 3) {
+      const row = [];
+  
+      for (let j = 0; j < 3; j++) {
+        const placeIndex = i + j;
+        if (placeIndex >= placeCategory.length) {
+          break;
+        }
+  
+        const place = placeCategory[placeIndex];
+        row.push(m.callbackButton(place, `select_place_category_${place}`));
+      }
+  
+      keyboard.push(row);
+    }
+    return m.inlineKeyboard(keyboard);
+});
+
+
+export const tgWrapper = () => {
+    bot.launch();
+    bot.start((ctx: any) => {
+        console.log("New user has joined");
+        state[ctx.from.id] = {};
+        ctx.reply(
+            `🔖 Welcome to the Tour Guide Bot dear *${ctx.from.first_name}*🔖 \n\n🕒 Time: ${new Date()} \n\n Kindly, choose a category of your choice:` 
+            ,placeCategoryMenu
+        );
+    });
+}
+
 
 
 
